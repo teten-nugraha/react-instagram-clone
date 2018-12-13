16a370c597b11706cbc4b2168fd73ab466219164
teten <tennugraha777@gmail.com>
2018-12-13 09:12:19 +0700

    list

---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+import "./Header.css"
+
+class Header extends Component {
+    
+    render() {
+        return (
+            <nav className="Nav">
+                <div className="Nav-menus">
+                    <div className="Nav-brand">
+                        <a className="Nav-brand-logo" href="/">
+                            Instagram
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+
+} 
+
+export default Header;
